0f45b50bcdd5c2fa8211f585fe3e447d2a667a61
2017398956 <2017398956@qq.com>
2018-11-15 18:37:29 +0800

    update

--- src/main/java/personal/nfl/java/demo/Main.java
+++ src/main/java/personal/nfl/java/demo/Main.java
@@ -8,7 +8,31 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        StringUtil.printSystemProperties();
+//        StringUtil.printSystemProperties();
+        int i = 0;
+        switch (i++) {
+            case 0:
+                System.out.println("=== " + i);
+                i--;
+                break;
+            case 1:
+                System.out.println("+++++ " + i);
+                i--;
+                break;
+        }
+        System.out.println("======= " + i);
+        String text = "2348729874";
+        String key = "8";
+
+        for (int index = 0; index < text.length() && index >= 0; ) {
+            int temp = text.indexOf(key, index);
+            if (temp >= 0) {
+                index = temp + 1;
+            } else {
+                break;
+            }
+            System.out.println(index);
+        }
     }
 
 }
